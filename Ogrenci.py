97246abdce301ed9622eff5e41a5471e11aa6808
salihdiril <53401677+salihdiril@users.noreply.github.com>
2021-10-29 12:14:13 +0300

    Delete Ogrenci.py

--- Ogrenci.py
+++
@@ -1,8 +0,0 @@
-class Student:
-
-    def __init__(self, name, surname, major, no, gpa):
-        self.name = name
-        self.surname = surname
-        self.major = major
-        self.no = no
-        self.gpa = gpa
